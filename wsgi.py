89a34e3c2afde89840117e34d65e6d85fc5c673f
tsmakalagy <tsmakalagy@users.noreply.github.com>
2019-03-15 12:08:27 +0300

    Update wsgi.py
    Change kotokillah

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World Kotokillah!"
+    return "Hello World Again!"
 
 if __name__ == "__main__":
     application.run()
